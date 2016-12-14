aa3ad61aea8cde5516a53cef43a38a3032277841
David Wang <david.wang34@gmail.com>
2016-12-13 21:13:40 -0500

    Initial Commit

---
+++ Location Journal/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Location Journal
+//
+//  Created by David Wang on 12/13/16.
+//  Copyright © 2016 David Wang. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
